be023b45713f4a77e6409ea2edbf36befdfe29de
pbaeyens <pbaeyens31@gmail.com>
2015-05-28 18:11:03 +0200

    Splines Natural y Sujerto

--- Octave/SplineNat.m
+++ Octave/SplineNat.m
@@ -1,15 +1,20 @@
 function s = SplineNat(x, y)
-  n = length(x) - 1
+  n = length(x) - 1;
 
-  h = diff(x);  #h_i
-  p = h./diff(y); #p_i
+  h = diff(x);     #h_i
+  p = diff(y)./h;  #p_i
 
-  u = h./(h + h(2:end)); # mu_i
-  v = ones(1,n) - u;     # lambda_i
-  g = diff(p)./(h + h(2:end))  # gamma_i
+  twoes  = 2*ones(1,n-1);
+  mu     = (h(1:end-1)./(h(1:end-1) + h(2:end)));
+  lambda = ones(1,n-1) - mu;
+  g      = diff(p)./(h(1:end-1) + h(2:end));
 
-  A = diag(2*ones(1,n+1)) + diag(v,1) + diag(u,-1)
+  if(n < 3)
+    A = 2;
+  else
+    A = diag(twoes) + diag(lambda(1:end-1),1) + diag(mu(2:end),-1);
+  end
+
   m = A \ g';
-
-  s = ppint(ppint(SplineLineal(x, m)));
+  s = ppint(ppint(SplineLineal(x, [0 m' 0])));
 end
--- Octave/SplineSuj.m
+++ Octave/SplineSuj.m
@@ -1,4 +1,22 @@
-function s = SplineSuj(x, y, d0, dn)
-  
-  s = ppint(ppint(SplineLineal(x, m)));
+function s = SplineSuj (x, y, d_1, d_n)
+  n     = length(x) - 1;
+  twoes = 2*ones(1,n+1);
+  h     = diff(x);
+
+  mu     = [(h(1:end-1)./(h(1:end-1) + h(2:end))) 1];
+  lambda = ones(1,n) - [0 mu(1:end-1)];
+
+  A = diag(mu,-1) + diag(twoes,0) + diag(lambda,1);
+
+  dd1 = diff(y)./h;
+  for i=1:(n-1)
+          dd2(i)=(dd1(i+1)-dd1(i))/(x(i+2)-x(i));
+  end
+
+  gamma(1)   = 6*(dd2(1)-d_1)/(x(2)-x(1));
+  gamma(2:n) = 6*dd2(1:n-1);
+  gamma(n+1) = 6*(d_n-dd2(n-1))/(x(n+1)-x(n));
+
+  m = A\gamma';
+  s = ppint(ppint(SplineLineal(x, m')));
 end
