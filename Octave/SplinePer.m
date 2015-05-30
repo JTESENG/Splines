8d689117245702d918de92e56a38c4c46d330696
Rubén Morales Pérez <srmorales@correo.ugr.es>
2015-05-30 11:06:33 +0200

    Update SplinePer.m

--- Octave/SplinePer.m
+++ Octave/SplinePer.m
@@ -1,42 +1,43 @@
-function s = SplinePer(x, y)
-  n = length(x);
-  twoes = 2*ones(1,n);
-  h = diff(x);
-  mu     = [(h(1:end-1)./(h(1:end-1) + h(2:end))) 1];
-  lambda = ones(1,n-1) - [0 mu(1:end-1)];
+function z = SplinePer (x, y) 
+    n=length(x);
+    twoes=2*ones(1,n);
+##    twoes[1] = twoes[1] +2;
+    h=diff(x);
+    lambda=zeros(1,n-1); ##
+    for i=1:(n-2) 
+    	lambda(i+1)=h(i+1)/(h(i+1)+h(i));
+    endfor
+    mu=zeros(1,n-1);
+    for i=1:(n-2)
+    	mu(i)=mu(i)+h(i)/(h(i+1)+h(i));
+    endfor
+#    mu(n-1)=1;
 
-  A = diag(mu,-1) + diag(twoes,0) + diag(lambda,1);
-  dd1 = (diff(y)./diff(x))';
+    A=1*diag(mu,-1)+1*diag(twoes)+1*diag(lambda,1); 
+    [fil col] = length(A);
+    A(1, fil) = h(n-1)/(h(n-1)+h(n-2));
+    
+    dd1=zeros(n-1,1);
+    for i=1:(n-1)
+   		dd1(i)=(y(i+1)-y(i))/(x(i+1)-x(i)); #Primeras derivadas
+    endfor
+    dd2=zeros(n-2,1);
+    for i=1:(n-2)
+    	dd2(i)=(dd1(i+1)-dd1(i))/(x(i+2)-x(i)); #Segundas derivadas
+    endfor
+    
+    A(1,:)=h(1)*A(1,:);   #Ajustes de la matriz
+    A(1,1)=A(1,1)-h(1)/3;
+    A(1,2)=A(1,2)+h(1)/6;
+    A(n,:)=h(n-1)*A(1,:);
+    A(n,1)=A(1,1)+h(1)/3;
+    A(n,2)=A(1,2)-h(1)/6;
+    gamma=zeros(n-1,1);
+    gamma(1)=(h(1)-dd1(1))/h(1) + (dd1(1)-h(n))/h(n);
+    for i=1:(n-2)
+    	gamma(i+1)=6*dd2(i);
+    endfor
+    m=A\gamma;
+    s = ppint(ppint(SplineLineal(x, m)));  
 
-  dd2=zeros(n-2,1);
-  for i=1:(n-2)
-          dd2(i)=(dd1(i+1)-dd1(i))/(x(i+2)-x(i));
-  end
-
-  A(1,:)=h(1)*A(1,:);
-  A(1,1)=A(1,1)-h(1)/3;
-  A(1,2)=A(1,2)+h(1)/6;
-  A(n,:)=h(n-1)*A(1,:);
-
-  A(n,1) = A(1,1)+h(1)/3;
-  A(n,2) = A(1,2)-h(1)/6;
-  gamma = zeros(n,1);
-  gamma(1)=h(1)-dd1(1);
-  gamma(n)=-h(n-1)+dd1(1);
-
-  for i=1:(n-2)
-    gamma(i+1)=6*dd2(i);
-  end
-
-  m = A\gamma;
-
-  for i = 1:(n-1)
-    p  = (-m(i)/6)   * poly([x(i+1), x(i+1), x(i+1)]);
-    p += (m(i+1)/6) .* poly([x(i), x(i), x(i)])
-    p += (y(i)-(m(i)*h(i).^2/6))*[0 0 -1 x(i+1)];
-    p += (y(i+1)-(m(i+1)*h(i).^2/6))*[0 0 1 -x(i)];
-    p *= 1/h(i);
-    B(i,:) = polyaffine(p, [-x(i) 1]);
-  end
-  s = mkpp(x, B);
-end
+endfunction
